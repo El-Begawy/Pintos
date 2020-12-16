1efb3924618b1084a744c74a9466159666cd9b49
ziad <zyad012ziad012@gmail.com>
2020-12-16 07:30:33 +0200

    Check priority on Sema_up

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -113,11 +113,18 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+  struct thread *t = NULL;
+  if (!list_empty (&sema->waiters)) {
+      t = list_entry (list_pop_front (&sema->waiters),
+                      struct thread, elem);
+      thread_unblock (t);
+  }
   sema->value++;
-  intr_set_level (old_level);
+  // check if unblocked thread has a higher priority
+  if(t != NULL && t->priority > thread_current()->priority)
+      thread_yield ();
+
+    intr_set_level (old_level);
 }
 
 static void sema_test_helper (void *sema_);
